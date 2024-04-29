83b8a9ab1801ce5bfdf7266e898284328905e4b8
JennnyMC <jennymccamley@gmail.com>
2024-04-29 12:06:11 +0200

    Update main.py
    importerat nytt skal från flet och lagt till navigationsrutter för knapparna, vi måste fortfarande utveckla själva sidorna.

--- main.py
+++ main.py
@@ -1,85 +1,114 @@
 import math
 import flet as ft
 
-
-
-
 def main(page: ft.Page):
-    
-    page.drawer = ft.NavigationDrawer(bgcolor="0xFFACE7F4",
-            controls=[
-            ft.Container(height=12),
-            ft.NavigationDrawerDestination(
-                label="Hem",
-                icon=ft.icons.DOOR_BACK_DOOR_OUTLINED,
-                selected_icon_content=ft.Icon(ft.icons.DOOR_BACK_DOOR),
-            ),
-            ft.Divider(thickness=2),
-            ft.NavigationDrawerDestination(
-                icon_content=ft.Icon(ft.icons.ARROW_FORWARD_IOS),
-                label="Åsiktsjämförelse", 
-                selected_icon=ft.icons.ARROW_FORWARD_IOS,
-            ),
-            ft.NavigationDrawerDestination(
-                icon_content=ft.Icon(ft.icons.ARROW_FORWARD_IOS),
-                label="Genomförda vallöften",
-                selected_icon=ft.icons.ARROW_FORWARD_IOS,
-            ),
-            ft.NavigationDrawerDestination(
-                icon_content=ft.Icon(ft.icons.ARROW_FORWARD_IOS),
-                label="Partiståndpunkter",
-                selected_icon=ft.icons.ARROW_FORWARD_IOS,
-            ),
-            ft.NavigationDrawerDestination(
-                icon_content=ft.Icon(ft.icons.ARROW_FORWARD_IOS),
-                label="Vad händer hos dig?",
-                selected_icon=ft.icons.ARROW_FORWARD_IOS,
-            ),
-            ft.NavigationDrawerDestination(
-                icon_content=ft.Icon(ft.icons.STAR_ROUNDED),
-                label="Quiz",
-                selected_icon=ft.icons.STAR,
-            ),
-            ft.NavigationDrawerDestination(
-                icon_content=ft.Icon(ft.icons.STAR_ROUNDED),
-                label="Topplista",
-                selected_icon=ft.icons.STAR,
-            )
-        ],
-    )
+    page.title = "Routes Example"
 
-    def show_drawer(e):
-        page.drawer.open = True
-        page.drawer.update()
-
-    page.add(ft.ElevatedButton("Show drawer", on_click=show_drawer))
-
-    page.add(
-        ft.Container(
-            alignment=ft.alignment.center,
-            gradient=ft.LinearGradient(
-                begin=ft.alignment.top_left,
-                end=ft.Alignment(0.8, 1),
-                colors=[
-                    "0xFF335D74",
-                    "0xFF3E728E",
-                    "0xFF60AEDA",
-                    "0xFF235774",
+   
+    def route_change(route):
+        page.views.clear()
+        page.views.append(
+            ft.View(
+                "/",
+                [
+                    ft.AppBar(title=ft.Text("Politik Pulsen"), bgcolor=ft.colors.SURFACE_VARIANT),
+                    ft.Container(
+                        content=ft.Column(
+                            [
+                                ft.ElevatedButton("Åsiktsjämförelse", on_click=lambda _: page.go("/compare")),
+                                ft.ElevatedButton("Genomförda Vallöften", on_click=lambda _: page.go("/stats")),
+                                ft.ElevatedButton("Partiståndpunkter", on_click=lambda _: page.go("/values")),
+                                ft.ElevatedButton("Vad Händer Hos Dig?", on_click=lambda _:page.go("/local")),
+                                ft.ElevatedButton("Quiz", on_click=lambda _:page.go("/quest")),
+                                ft.ElevatedButton("Topplista", on_click=lambda _:page.go("/toplist"))
+                            ]
+                        ),
+                        bgcolor = ft.colors.BLUE_200,
+                        border_radius= 5
+                    )
                 ],
-                tile_mode=ft.GradientTileMode.MIRROR,
-                rotation=math.pi / 3,
-            ),
-            width=393,
-            height=652,
-            border_radius=5,
-            content=ft.Stack(
-                controls=[
-                
-
-                ],
-                
             )
         )
-    )
+        if page.route == "/compare":
+            page.views.append(
+                ft.View(
+                    "/compare",
+                    [
+                        ft.AppBar(title=ft.Text("Jämför Ståndpunkter"), bgcolor=ft.colors.SURFACE_VARIANT),
+                        ft.ElevatedButton("Go Home", on_click=lambda _: page.go("/")),
+                    ],
+                )
+            )
+        page.update()
+        if page.route == "/stats":
+            page.views.append(
+                ft.View(
+                    "/stats",
+                    [
+                        ft.AppBar(title=ft.Text("Genomförda vallöften"), bgcolor=ft.colors.SURFACE_VARIANT),
+                        ft.ElevatedButton("Go Home", on_click=lambda _: page.go("/")),
+                    ],
+                )
+            )
+        page.update()
+
+        if page.route == "/values":
+            page.views.append(
+                ft.View(
+                    "/values",
+                    [
+                        ft.AppBar(title=ft.Text("Partiståndpunkter"), bgcolor=ft.colors.SURFACE_VARIANT),
+                        ft.ElevatedButton("Go Home", on_click=lambda _: page.go("/")),
+                    ]
+
+                )
+            )
+        page.update()
+
+        if page.route == "/local":
+            page.views.append(
+                ft.View(
+                    "/local",
+                    [
+                        ft.AppBar(title=ft.Text("Vad Händer Hos Dig?"), bgcolor=ft.colors.SURFACE_VARIANT),
+                        ft.ElevatedButton("Go Home", on_click=lambda _: page.go("/")),
+                    ]
+                )
+            )
+        page.update()
+    
+        if page.route == "/quest":
+            page.views.append(
+                ft.View(
+                    "/quest",
+                    [
+                        ft.AppBar(title=ft.Text("Quiz"), bgcolor=ft.colors.SURFACE_VARIANT),
+                        ft.ElevatedButton("Go Home", on_click=lambda _: page.go("/")),
+                    ]
+                )
+            )
+        page.update()
+
+        if page.route == "/toplist":
+            page.views.append(
+                ft.View(
+                    "/toplist",
+                    [
+                        ft.AppBar(title=ft.Text("Topplista"), bgcolor=ft.colors.SURFACE_VARIANT),
+                        ft.ElevatedButton("Go Home", on_click=lambda _: page.go("/")),
+                    ]
+                )
+            )
+        page.update()
+
+    def view_pop(view):
+        page.views.pop()
+        top_view = page.views[-1]
+        page.go(top_view.route)
+
+    page.on_route_change = route_change
+    page.on_view_pop = view_pop
+    page.go(page.route)
+
 
 ft.app(target=main)
