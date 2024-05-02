33a7acf2cd489190668cc8be2c1b658cadca2b10
JennnyMC <jennymccamley@gmail.com>
2024-05-02 10:01:33 +0200

    Create py.py

---
+++ py.py
@@ -0,0 +1,160 @@
+import math
+import flet as ft
+
+def main(page: ft.Page):
+    page.title = "PolitikPulsen"
+    BG = "#1A5577"
+    MC = "#60AEDA"
+   
+
+    def route_change(route):
+        page.views.clear()
+        page.views.append(
+            ft.View(
+                "/",
+                [
+                    ft.AppBar(title=ft.Text("PolitikPulsen"), bgcolor=MC),
+                    ft.Container(
+                        content=ft.Column(
+                            [
+                                ft.ElevatedButton("Åsiktsjämförelse",icon="ARROW_FORWARD_IOS", on_click=lambda _: page.go("/compare")),
+                                ft.ElevatedButton("Genomförda Vallöften", icon="ARROW_FORWARD_IOS", on_click=lambda _: page.go("/stats")),
+                                ft.ElevatedButton("Partiståndpunkter", icon="ARROW_FORWARD_IOS", on_click=lambda _: page.go("/values")),
+                                ft.ElevatedButton("Vad Händer Hos Dig?", icon="ARROW_FORWARD_IOS", on_click=lambda _:page.go("/local")),
+                                ft.ElevatedButton("Quiz", icon="STAR_ROUNDED", on_click=lambda _:page.go("/quest")),
+                                ft.ElevatedButton("Topplista", icon="STAR_ROUNDED", on_click=lambda _:page.go("/toplist"))
+                            ],
+                            alignment=ft.MainAxisAlignment.CENTER,
+                        ),
+                        bgcolor = BG,
+                        border_radius=35,
+                        width=350,
+                        height=700,
+                        alignment=ft.alignment.center
+                    )
+                ],
+            )
+        )
+        if page.route == "/compare":
+            page.views.append(
+                ft.View(
+                    "/compare",
+                    [
+                        ft.AppBar(title=ft.Text("Jämför Ståndpunkter"), bgcolor=MC),
+                        ft.ElevatedButton("Go Home", on_click=lambda _: page.go("/")),
+                        ft.Container(
+                            width=350,
+                            height=700,
+                            bgcolor=BG,
+                            border_radius=35
+                        )
+                    ],
+                )
+            )
+        page.update()
+        if page.route == "/stats":
+            page.views.append(
+                ft.View(
+                    "/stats",
+                    [
+                        ft.AppBar(title=ft.Text("Genomförda vallöften"), bgcolor=MC),
+                        ft.ElevatedButton("Go Home", on_click=lambda _: page.go("/")),
+                        ft.Container(
+                            width=350,
+                            height=700,
+                            bgcolor=BG,
+                            border_radius=35
+                        )
+                    ],
+                )
+            )
+        page.update()
+
+        if page.route == "/values":
+            page.views.append(
+                ft.View(
+                    "/values",
+                    [
+                        ft.AppBar(title=ft.Text("Partiståndpunkter"), bgcolor=MC),
+                        ft.ElevatedButton("Go Home", on_click=lambda _: page.go("/")),
+                        ft.Container(
+                            width=350,
+                            height=700,
+                            bgcolor=BG,
+                            border_radius=35,
+                            content=ft.Stack(
+                                controls=[ft.Text]
+                            )
+                        )
+                        
+                    ]
+
+                )
+            )
+        page.update()
+
+        if page.route == "/local":
+            page.views.append(
+                ft.View(
+                    "/local",
+                    [
+                        ft.AppBar(title=ft.Text("Vad Händer Hos Dig?"), bgcolor=MC),
+                        ft.ElevatedButton("Go Home", on_click=lambda _: page.go("/")),
+                        ft.Container(
+                            width=350,
+                            height=700,
+                            bgcolor=BG,
+                            border_radius=35
+                        )
+                    ]
+                )
+            )
+        page.update()
+    
+        if page.route == "/quest":
+            page.views.append(
+                ft.View(
+                    "/quest",
+                    [
+                        ft.AppBar(title=ft.Text("Quiz"), bgcolor=MC),
+                        ft.ElevatedButton("Go Home", on_click=lambda _: page.go("/")),
+                        ft.Container(
+                            width=350,
+                            height=700,
+                            bgcolor=BG,
+                            border_radius=35
+                        )
+                    ]
+                )
+            )
+        page.update()
+
+        if page.route == "/toplist":
+            page.views.append(
+                ft.View(
+                    "/toplist",
+                    [
+                        ft.AppBar(title=ft.Text("Topplista"), bgcolor=MC),
+                        ft.ElevatedButton("Go Home", on_click=lambda _: page.go("/")),
+                        ft.Container(
+                            width=350,
+                            height=700,
+                            bgcolor=BG,
+                            border_radius=35
+                        )
+                    ]
+                )
+            )
+        page.update()
+
+    def view_pop(view):
+        page.views.pop()
+        top_view = page.views[-1]
+        page.go(top_view.route)
+
+    page.on_route_change = route_change
+    page.on_view_pop = view_pop
+    page.go(page.route)
+
+
+ft.app(target=main)
